0d087f1d24a201b17418e1855149053bd61997da
aryanghbd <54407419+aryanghbd@users.noreply.github.com>
2021-08-11 15:14:42 -0400

    Updated

---
+++ assets/js/extra.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var typed = new Typed(".typing", {
+        strings: ["Backend development", "Assembly level design", "Compiler and CPU Engineering", "IoT Technology", "Computer Vision", "Machine Learning", "App Development", "Embedded Systems development", "Networking", "Cloud Computing", "Full-Stack Development"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
