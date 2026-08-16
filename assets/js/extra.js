7fc6b49259e918cef446812120b7ba253a1554e1
Aryan Ghobadi <54407419+aryanghbd@users.noreply.github.com>
2026-08-16 12:17:58 -0400

    Delete assets directory

--- assets/js/extra.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var typed = new Typed(".typing", {
-        strings: ["Backend development", "Low-latency optimisation", "Compilers", "Embedded Systems", "Full-stack development", "Full scale application-development", "FPGA Development", "Networking", "Cloud Computing"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-});
